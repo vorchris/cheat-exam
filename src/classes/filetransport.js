640a30694e29dd1ccbf6864cb32b547d02f8c6e7
valueerror <valueerror@gmail.com>
2021-04-25 23:14:11 +0200

    cleanup new filetransfer classes

--- src/classes/filetransport.js
+++ src/classes/filetransport.js
@@ -1,43 +1,16 @@
 const net = require('net')
 const fs = require('fs')
-const remoteserver = process.argv[2]
-
-class transportReceiver {
-  constructor () {
-    this.date = new Date()
-    this.size = 0
-    this.elapsed = 0
-  }
-
-  init (filename) {
-    this.socket = remoteserver ? net.connect(8000, remoteserver) : net.connect(8000)
-    this.ostream = fs.createWriteStream(filename)
-
-    this.socket.on('data', chunk => {
-      this.size += chunk.length
-      this.elapsed = new Date() - this.date
-      this.socket.write(`\r${(this.size / (1024 * 1024)).toFixed(2)} MB of data was sent. Total elapsed time is ${this.elapsed / 1000} s`)
-      process.stdout.write(`\r${(this.size / (1024 * 1024)).toFixed(2)} MB of data was sent. Total elapsed time is ${this.elapsed / 1000} s`)
-      this.ostream.write(chunk)
-    })
-
-    this.socket.on('end', () => {
-      console.log(`\nFinished getting file. speed was: ${((this.size / (1024 * 1024)) / (this.elapsed / 1000)).toFixed(2)} MB/s`)
-      process.exit()
-    })
-  }
-}
 
 class transportSender {
   constructor () {
     this.istream = false
+    this.server = false
   }
 
   init (filename) {
     this.istream = fs.createReadStream(filename)
-    const server = net.createServer(socket => {
-      socket.pipe(process.stdout)
 
+    this.server = net.createServer(socket => {
       this.istream.on('readable', function () {
         let data
         // eslint-disable-next-line no-cond-assign
@@ -47,15 +20,38 @@ class transportSender {
       })
 
       this.istream.on('end', function () {
-        socket.end()
+        socket.end() // terminates socket on both ends
       })
 
       socket.on('end', () => {
-        server.close(() => { console.log('\nTransfer is done!') })
+        this.server.close(() => { console.log('\n Transfer is done!') })
       })
     })
 
-    server.listen(8000, '0.0.0.0')
+    this.server.listen(8000, '0.0.0.0')
+  }
+}
+
+class transportReceiver {
+  constructor () {
+    this.date = new Date()
+    this.size = 0
+    this.elapsed = 0
+  }
+
+  init (filename) {
+    this.socket = net.connect(8000, 'localhost') // localhost to variable
+    this.ostream = fs.createWriteStream(filename)
+
+    this.socket.on('data', chunk => {
+      this.size += chunk.length
+      this.elapsed = new Date() - this.date
+      this.ostream.write(chunk)
+    })
+
+    this.socket.on('end', () => {
+      console.log(`\n Finished getting file. Speed was: ${((this.size / (1024 * 1024)) / (this.elapsed / 1000)).toFixed(6)} MB/s`)
+    })
   }
 }
 
