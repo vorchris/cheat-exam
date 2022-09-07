16d4aa2582caf8802569dbb049dca5cbe6599b4c
valueerror <valueerror@gmail.com>
2022-09-07 12:47:28 +0200

    fixed crash on special chars like öüä in exam name

--- teacher/packages/server/src/classes/multicastclient.js
+++ teacher/packages/server/src/classes/multicastclient.js
@@ -34,9 +34,6 @@ class MulticastClient {
      * receives messages and stores new exam instances in this.examServerList[]
      */
     messageReceived (message, rinfo) {
-    
-        console.log(String(message))
-        
         const serverInfo = JSON.parse(String(message))
         serverInfo.serverip = rinfo.address
         serverInfo.serverport = rinfo.port
