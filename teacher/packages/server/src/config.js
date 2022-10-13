ec1f6d9ac3370301dea84c4af1cd8b956b5bf318
valueerror <valueerror@gmail.com>
2022-10-13 16:57:04 +0200

    deactivated dev mode

--- teacher/packages/server/src/config.js
+++ teacher/packages/server/src/config.js
@@ -1,8 +1,8 @@
 import pjson from "../../../package.json"
 
 const config = {
-    development: true,  // disable kiosk mode on exam mode and other stuff (autofill input fields)
-    showdevtools:true,
+    development: false,  // disable kiosk mode on exam mode and other stuff (autofill input fields)
+    showdevtools: true,
 
     version: pjson.version,
     workdirectory : "",   // set by server.js (desktop path + examdir)
