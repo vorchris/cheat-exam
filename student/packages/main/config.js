38e77e2486e67f19011f380e4524f7163991744a
valueerror <valueerror@gmail.com>
2022-11-03 22:28:59 +0100

    added zoom svg

--- student/packages/main/config.js
+++ student/packages/main/config.js
@@ -2,7 +2,7 @@ import pjson from "../../package.json"
 
 
 const config = {
-    development: true,  // disable kiosk mode on exam mode and other stuff (autofill input fields)
+    development: false,  // disable kiosk mode on exam mode and other stuff (autofill input fields)
     showdevtools: true,
 
     version: pjson.version,
