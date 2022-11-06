7daf4f19a6c2cca0748dc1c56b942581631a9507
valueerror <valueerror@gmail.com>
2022-11-06 19:07:26 +0100

    version&config

--- teacher/packages/server/src/config.js
+++ teacher/packages/server/src/config.js
@@ -2,7 +2,7 @@ import pjson from "../../../package.json"
 
 const config = {
     development: false,  // disable kiosk mode on exam mode and other stuff (autofill input fields)
-    showdevtools: true,
+    showdevtools: false,
     buildforWEB :false,
 
     version: pjson.version,
