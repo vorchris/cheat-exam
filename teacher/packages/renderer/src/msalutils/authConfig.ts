be814d83614cb0622279b7fb18e65b59f749f5dc
valueerror <valueerror@gmail.com>
2023-03-23 10:10:05 +0100

    playing around with onedrive api

---
+++ teacher/packages/renderer/src/msalutils/authConfig.ts
@@ -0,0 +1,52 @@
+import { LogLevel, PublicClientApplication } from '@azure/msal-browser';
+
+// Config object to be passed to Msal on creation
+export const msalConfig = {
+  auth: {
+    clientId: 'c952edde-d7c2-4281-a846-034fb039e1f5',
+    authority: 'https://login.microsoftonline.com/common',
+    redirectUri: 'https://localhost:22422/server/control/msauth/',
+    postLogoutRedirectUri: 'https://localhost:22422/server/control/msauth/'
+  },
+  cache: {
+    cacheLocation: 'localStorage'
+  },
+  system: {
+      loggerOptions: {
+          loggerCallback: (level: LogLevel, message: string, containsPii: boolean) => {
+              if (containsPii) {
+                  return;
+              }
+              switch (level) {
+                  case LogLevel.Error:
+                      console.error(message);
+                      return;
+                  case LogLevel.Info:
+                      console.info(message);
+                      return;
+                  case LogLevel.Verbose:
+                      console.debug(message);
+                      return;
+                  case LogLevel.Warning:
+                      console.warn(message);
+                      return;
+                  default:
+                      return;
+              }
+          },
+          logLevel: LogLevel.Verbose
+      }
+  }
+};
+
+export const msalInstance = new PublicClientApplication(msalConfig);
+
+// Add here scopes for id token to be used at MS Identity Platform endpoints.
+export const loginRequest = {
+  scopes: ['User.Read'],
+};
+
+// Add here the endpoints for MS Graph API services you would like to use.
+export const graphConfig = {
+  graphMeEndpoint: 'https://graph.microsoft.com/v1.0/me',
+};
