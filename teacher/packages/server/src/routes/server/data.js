df460c1efaed156fdae80f424278c36042e0b4e6
valueerror <valueerror@gmail.com>
2023-11-06 10:53:08 +0100

    copyright

--- teacher/packages/server/src/routes/server/data.js
+++ teacher/packages/server/src/routes/server/data.js
@@ -1,7 +1,7 @@
 
 /**
  * @license GPL LICENSE
- * Copyright (c) 2021-2023 Thomas Michael Weissel
+ * Copyright (c) 2021 Thomas Michael Weissel
  * 
  * This program is free software: you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the Free Software Foundation,
