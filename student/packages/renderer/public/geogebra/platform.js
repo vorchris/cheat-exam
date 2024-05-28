60b4f86cac18e9d32e5bbcd59b83fa2b43a3d199
valueerror <valueerror@gmail.com>
2024-05-28 22:53:20 +0200

    update geogebra suite and classic

--- student/packages/renderer/public/geogebra/platform.js
+++ student/packages/renderer/public/geogebra/platform.js
@@ -1,51 +1,37 @@
+/* eslint-disable-next-line no-unused-vars */
+/* globals defaultParams, appOnline:writable, startDelay:writable */
 defaultParams.laf = "bundle";
-analyticsVersion = 'V'+latestVersion.replace(/^5/,"6")+"-offline";
 startDelay = 500;
 appOnline = false;
 
-
-
-//https://gist.github.com/paulcbetts/2d2de55d137a1cf9d1ac
-// function installAnalytics(gaID){
-// 	// grab a uuid to identify the session
-// 	// set the uuid to local storage
-// 	if (!localStorage["gaClientId"]){
-// 	    localStorage.setItem("gaClientId","R"+Math.round(Math.random()*1E12)*1000+new Date().getMilliseconds());
-// 	}
-// 	// set the cookie storage for google analytics to none, and provide your own client id
-// 	var clientId = localStorage["gaClientId"];
-// 	ga('create', gaID,{
-// 	    'storage': 'none',
-// 	    'clientId': clientId
-// 	});
-// 	// finally, disable the protocol check to allow file://
-// 	ga('set', 'checkProtocolTask', function(){}); // Disable file protocol checking.
-// }
-
+/* eslint-disable-next-line no-unused-vars */
 function ggbExamMode(exam) {
-	//window.ipc.send("exam", exam);
+    return
 }
 
+/* eslint-disable-next-line no-unused-vars */
 function setUnsavedMessage(message, save, noSave, cancel){
-	//window.ipc.send('unsaved', JSON.stringify([message,save,noSave,cancel]));
+   return
 }
 
-console.log = function(message) {
-      //  window.ipc.sendSync('log', message);
-}
 
-function copyGraphicsToClipboardExternal(data) {
-	//return window.ipc.sendSync('clipboard', data);
-}
 
 const defaultOpen = window.open;
 
 window.open = function(url, features) {
-	if (url.match('geogebra.org')) {
-		return defaultOpen(url, features);
-		console.log("Opening URL " + url + " inside electron app");
-	} else {
-		//window.ipc.send('openUrl', url);
-		console.log("Opening URL " + url + " in external OS browser");
-	}
+    return
+}
+
+const blobToBase64 = blob => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    return new Promise(resolve => {
+        reader.onloadend = () => {
+            resolve(reader.result.split('base64,')[1]);
+        };
+    });
+};
+
+window.showSaveFilePicker = function(options) {
+    return
 }
