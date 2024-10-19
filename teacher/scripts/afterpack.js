796f08bdb33d69b029eb50f5b691231005c7c787
valueerror <valueerror@gmail.com>
2024-10-19 17:16:36 +0200

    mac finally builds

--- teacher/scripts/afterpack.js
+++ teacher/scripts/afterpack.js
@@ -1,25 +1,23 @@
-// after-pack.js
-const path = require('path');
-const fs = require('fs-extra'); // Paket zum einfachen Löschen von Verzeichnissen
+import path from 'path';
+import fs from 'fs-extra';
 
-module.exports = async (context) => {
-  const arch = context.arch; // Architektur ermitteln (x64 oder arm64)
-  const appPath = context.appOutDir; // Pfad zum App-Ausgabeverzeichnis
+export default async function afterPack(context) {
+  const arch = context.arch;
+  const appPath = context.appOutDir;
 
-  // Pfade zu den Verzeichnissen, die entfernt werden sollen
   const x64JrePath = path.join(appPath, 'public', 'minimal-jre-11-mac-arm64');
   const arm64JrePath = path.join(appPath, 'public', 'minimal-jre-11-mac');
 
-  // Architekturprüfung und entsprechende Verzeichnisse entfernen
   if (arch === 'x64') {
-    if (fs.existsSync(x64JrePath)) {
+    if (await fs.pathExists(x64JrePath)) {
       await fs.remove(x64JrePath);
       console.log(`Removed ARM64 JRE from x64 build: ${x64JrePath}`);
     }
   } else if (arch === 'arm64') {
-    if (fs.existsSync(arm64JrePath)) {
+    if (await fs.pathExists(arm64JrePath)) {
       await fs.remove(arm64JrePath);
       console.log(`Removed x64 JRE from ARM64 build: ${arm64JrePath}`);
     }
   }
-};
+}
+
