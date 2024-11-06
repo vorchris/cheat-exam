ff857f7a07480b0185a295871c731a9d421bce91
valueerror <valueerror@gmail.com>
2024-11-06 14:58:16 +0100

    remove transparent window on startup because windows bugs around with it

--- student/packages/main/scripts/windowhandler.js
+++ student/packages/main/scripts/windowhandler.js
@@ -345,7 +345,7 @@ class WindowHandler {
             visibleOnAllWorkspaces: true,
             kiosk: true,
             show: false,
-            transparent: true,
+            //transparent: true,
             icon: join(__dirname, '../../public/icons/icon.png'),
             webPreferences: {
                 preload: join(__dirname, '../preload/preload.cjs'),
@@ -373,16 +373,20 @@ class WindowHandler {
 
                 // this.examwindow.setMinimizable(false)
                 // this.examwindow.setVisibleOnAllWorkspaces(true); 
-                
+                // this.examwindow.setFullScreen(true)
+
                 this.examwindow.focus();
                 this.examwindow.setKiosk(true); 
                 this.examwindow.setOpacity(1)
+
                 //restrictions
                 this.addBlurListener();  // detects if window gets out of focus 
                 if (!this.isWayland){ this.checkWindowInterval.start() }  // checks if the active window is next-exam (introduces exceptions for windows) 
                 enableRestrictions(this)  // enable restriction only when exam window is fully loaded and in focus
                 await this.sleep(2000)    // wait an additional 2 sec for windows restrictions to kick in (they steal focus)
                 this.examwindow.focus();  // focus again just to be sure
+               
+
             }
         })
 
